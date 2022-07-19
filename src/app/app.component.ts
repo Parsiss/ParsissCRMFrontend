2e4a7c63de5b04372b601484f0852d2a9ca63e0e
MohammadRezaKashi <mohammadreza.kashi.1992@gmail.com>
2022-07-19 08:53:27 +0430

    Reformat some files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,16 +9,16 @@ import { TranslateService } from '@ngx-translate/core';
 export class AppComponent {
   title = 'ParsissCRMFrontend';
   private _selectedLanguage = 'fa';
-  
+
   textDir: 'rtl' | 'ltr' = 'rtl';
 
   get selectedLanguage(): string {
-      return this._selectedLanguage;
+    return this._selectedLanguage;
   }
   set selectedLanguage(value: string) {
-      this._selectedLanguage = value;
-      this.textDir = this._selectedLanguage === 'fa' ? 'rtl' : 'ltr';
-      this.translate.use(value);
+    this._selectedLanguage = value;
+    this.textDir = this._selectedLanguage === 'fa' ? 'rtl' : 'ltr';
+    this.translate.use(value);
   }
 
   constructor(public translate: TranslateService) {
